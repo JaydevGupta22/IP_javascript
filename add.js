4b2bed067464a7a2e6346d5c1f30b0c10ae9c599
Jaydev Gupta <jaidevgupta2004@gmail.com>
2025-01-25 15:55:15 +0530

    Second Commit

---
+++ add.js
@@ -0,0 +1,12 @@
+function add(){
+    x = 50
+    y = 60
+    z = x + y
+    
+    console.log("5 + 6 = 11");
+    console.log(x + " + " + y + " = " + z);
+    console.log(`${x} + ${y} = ${x+y}`);
+}
+
+add(5,6)
+add(50,60)
